18adb191be196eb3bdef8af076584219fc63ef39
kfischer-dev <kevin.fischer1992@googlemail.com>
2026-06-29 12:52:32 +0200

    feat: add station detail endpoint

--- src/api.py
+++ src/api.py
@@ -1,7 +1,7 @@
 import logging
 from src.logging_config import configure_logging
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status, Request
 from src.database import get_connection, fetch_stations, fetch_joined_measurements
 
 configure_logging()
@@ -41,6 +41,32 @@ def get_stations():
         logger.info("Database connection closed.")
         logger.info("=" * 60)
     return station_data
+
+@app.get("/stations/{station_id}")
+def get_stations_by_id(station_id: int):
+    logger.info("=" * 60)
+    logger.info("GET /stations/{station_id} request received. Opening database connection.")
+
+    conn = get_connection()
+
+    logger.info("Loading station data from database.")
+
+    try:
+        station_data = fetch_stations(conn)
+        logger.info(f"Loaded {len(station_data)} stations from database.")
+
+        for station in station_data:
+            if station.get("station_id") == station_id:
+                logger.info(f"Station: {station['station_name']} successfully loaded from database.")
+                return station
+        
+        logger.warning(f"Station with id {station_id} not found.")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Station with id {station_id} not found")
+
+    finally:
+        conn.close()
+        logger.info("Database connection closed.")
+        logger.info("=" * 60)
 
 @app.get("/measurements")
 def get_measurements():
--- src/logging_config.py
+++ src/logging_config.py
@@ -3,11 +3,11 @@ import logging
 
 def configure_logging():
     Path("logs").mkdir(exist_ok=True)
-    
+
     logging.basicConfig(filename="logs/app.log", level=logging.DEBUG,
                         format="%(asctime)s - %(levelname)s - %(module)s.%(funcName)s - %(message)s")
     
     logging.getLogger("uvicorn").setLevel(logging.WARNING)
     logging.getLogger("uvicorn.error").setLevel(logging.WARNING)
-    logging.getLogger("uvicorn.access").setLevel(logging.WARNING)
+    logging.getLogger("uvicorn.access").setLevel(logging.INFO)
     logging.getLogger("watchfiles").setLevel(logging.WARNING)
