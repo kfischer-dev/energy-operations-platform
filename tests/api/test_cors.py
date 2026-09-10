59066f5e05cdfe7a5cfd1998769de50eadb20476
kfischer-dev <kevin.fischer1992@googlemail.com>
2026-09-10 08:56:22 +0200

    test(api): add CORS preflight test

---
+++ tests/api/test_cors.py
@@ -0,0 +1,23 @@
+import pytest
+from fastapi.testclient import TestClient
+from src.api import app
+
+
+@pytest.mark.api
+@pytest.mark.cors
+def test_cors_headers():
+    client = TestClient(app)
+
+    response = client.options(
+        "/assets",
+        headers={
+            "Origin": "http://localhost:5173",
+            "Access-Control-Request-Method": "GET",
+        },
+    )
+
+    assert response.status_code == 200
+    assert response.headers.get("access-control-allow-origin") == (
+        "http://localhost:5173"
+    )
+    assert "GET" in response.headers.get("access-control-allow-methods", "")
